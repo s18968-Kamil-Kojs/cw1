9c49d74e3031bfd99e3598aea2b2fe39a5eee96d
s18968-Kamil-Kojs <s18968@pjwstk.edu.pl>
2020-03-02 17:05:24 +0100

    Dodalem projekt

---
+++ cw1/cw1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cw1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
